977a9fb674177412153cb56281e558327b7a5099
Cameron Cruickshank <cameron_cruickshank@cooperators.ca>
2019-08-22 16:55:27 +0000

    5th time...

--- src/main/java/catalog/CatalogController.java
+++ src/main/java/catalog/CatalogController.java
@@ -1,16 +1,17 @@
 package catalog;
 
+import catalog.models.*;
+
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import catalog.models.*;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * REST Controller to manage Inventory database
@@ -20,27 +21,28 @@ public class CatalogController {
 
     Logger logger = LoggerFactory.getLogger(CatalogController.class);
 
-	@Autowired
-	InventoryRepo itemsRepo;
-	
+    @Autowired
+    InventoryRepo itemsRepo;
+
     /**
      * @return all items in inventory
      */
-    @RequestMapping(value = "/items", method =
-RequestMethod.GET)
-@ResponseBody
-Iterable<Inventory> getInventory() {
-return itemsRepo.findAll();
-}
+    @RequestMapping(value = "/items", method = RequestMethod.GET)
+    @ResponseBody
+    Iterable<Inventory> getInventory() {
+        return itemsRepo.findAll();
+    }
 
-@RequestMapping(value = "/items/{id}", method =
-RequestMethod.GET)
-ResponseEntity<?> getById(@PathVariable long id) {
-if (!itemsRepo.exists(id)) {
-return ResponseEntity.notFound().build();
-}
-return
-ResponseEntity.ok(itemsRepo.findOne(id));
-}
+    /**
+     * @return item by id
+     */
+    @RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
+    ResponseEntity<?> getById(@PathVariable long id) {
+                if (!itemsRepo.exists(id)) {
+                        return ResponseEntity.notFound().build();
+                }
+
+                return ResponseEntity.ok(itemsRepo.findOne(id));
+    }
 
 }
